dbe158356c0e8e0d37cafca567ddd3398291567d
Felipe Correa <felipe.correa@gmail.com>
2017-10-25 18:39:49 -0200

    Initial Setup of my first 2D Game

---
+++ Assets/Scripts/Behaviors/Attack.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : AbstractBehavior
+{
+    public bool isAttacking = false;
+
+    protected virtual void Update()
+    {
+        isAttacking = inputState.GetButtonValue(inputButtons[0]);
+    }
+
+}
