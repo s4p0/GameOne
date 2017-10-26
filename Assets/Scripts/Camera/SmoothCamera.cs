708059341c115ff1add7f30bc9ec5276fcfe381e
s4p0 <felipe.correa@gmail.com>
2017-10-26 00:11:16 -0200

    adds smooth camera feature

---
+++ Assets/Scripts/Camera/SmoothCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothCamera : MonoBehaviour {
+
+	public float dampTime = 0.15f;
+	private Vector3 velocity = Vector3.zero;
+	public Transform target;
+	public float xOffset = 0;
+	public float yOffset = 0;
+	
+	// Update is called once per frame
+	void Update () {
+		if (target)
+		{
+			var offset = new Vector3 (xOffset, yOffset, 0);
+			Vector3 point = Camera.main.WorldToViewportPoint(target.position);
+			Vector3 delta = target.position - Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+			Vector3 destination = transform.position + delta;
+			transform.position = Vector3.SmoothDamp(transform.position, destination + offset, ref velocity, dampTime);
+		}
+	}
+}
